6223f2a2ee39544d09acc8066359c6b7af625e69
MMD1358 <xdeadx1358@gmail.com>
2026-05-18 19:48:31 +0200

    CRITTIC frontend v1.5

--- src/environment/environment.prod.ts
+++ src/environment/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  apiUrl: 'https://crittic-backend-production.up.railway.app//api/v1',
-  imageUrl: 'https://crittic-backend-production.up.railway.app//api/images'
+  apiUrl: 'https://crittic-backend-production.up.railway.app/api/v1',
+  imageUrl: 'https://crittic-backend-production.up.railway.app/api/images'
 };
